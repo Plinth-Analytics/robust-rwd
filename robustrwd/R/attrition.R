6a9530db75f69f28075a8619129193cf82fb46f3
Mustafa Ascha <mustafa@plinth.co>
2022-10-21 16:50:26 -0700

    start package, add attrition counter

---
+++ robustrwd/R/attrition.R
@@ -0,0 +1,25 @@
+
+
+step_counter_factory <- function(.df) {
+  function(criterion, name) {
+    .df <<- filter(.df, !!criterion)
+    mutate(count(.df), description = name)
+  }
+}
+
+#' Count how many rows are filtered out of a dataframe (sequentially)
+#' 
+#' @param .df A table whose rows will be counted
+#' @param ... Criteria (named, if you like) to apply to `.df`
+#' 
+#' @example step_counter(mtcars, "vs are 1" = vs == 1, "mpg less than 20" = mpg < 20)
+step_counter <- function(.df, ...) {
+  # there's something nice about writing "Everyone" = TRUE
+  criteria <- append(list("Everyone" = quo(TRUE)), enquos(...))
+  counter <- step_counter_factory(.df)
+  reduce2(criteria, names(criteria), function(acc, cr, n_cr) {
+    bind_rows(acc, counter(cr, n_cr))
+  }, .init = tibble(description = character(0), n = integer(0)))
+}
+
+
