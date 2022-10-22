aed56a33f0219850e1df48efef607f8def628d8c
Mustafa Ascha <mustafa@plinth.co>
2022-10-22 14:11:35 -0700

    start effects injection code

---
+++ robustrwd/R/effects-injection.R
@@ -0,0 +1,13 @@
+
+# NOTE: injection works with tables that have gone through initial ETL
+
+# effects to be injected:
+# * make diabetes status missing for patients with diabetes who live for a shorter time
+# *
+
+inject_bene_effects <- function(bene_df) {
+
+}
+
+
+
