03301a6baaf67d0305fe2d8b61477fbb779a3138
Mustafa Ascha <mustafa@plinth.co>
2022-10-22 12:59:05 -0700

    ensure initial ETL returns something

--- robustrwd/R/initial-etl.R
+++ robustrwd/R/initial-etl.R
@@ -2,10 +2,11 @@
 initial_etl <- etl_factory(initial_etl_bene, initial_etl_inpatient)
 
 initial_etl_bene <- function(bene_df) {
-
+  bene_df 
+    
 }
 
 initial_etl_inpatient <- function(inpatient_df) {
-
+  inpatient_df
 }
 
