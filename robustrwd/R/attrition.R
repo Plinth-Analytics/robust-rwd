4edc64829f84914ae8a119dcf3bc37ddf08b3b43
Mustafa Ascha <mustafa@plinth.co>
2022-10-22 17:22:04 -0700

    add some spaces for easier reading

--- robustrwd/R/attrition.R
+++ robustrwd/R/attrition.R
@@ -8,15 +8,16 @@ step_counter_factory <- function(.df) {
 }
 
 #' Count how many rows are filtered out of a dataframe (sequentially)
-#' 
+#'
 #' @param .df A table whose rows will be counted
 #' @param ... Criteria (named, if you like) to apply to `.df`
-#' 
+#'
 #' @examples step_counter(mtcars, "vs are 1" = vs == 1, "mpg less than 20" = mpg < 20)
 step_counter <- function(.df, ...) {
   # there's something nice about writing "Everyone" = TRUE
   criteria <- append(list("Everyone" = quo(TRUE)), enquos(...))
   counter <- step_counter_factory(.df)
+
   reduce2(criteria, names(criteria), function(acc, cr, n_cr) {
     bind_rows(acc, counter(cr, n_cr))
   }, .init = tibble(description = character(0), n = integer(0)))
